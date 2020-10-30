521d950b7c42f8a3ac7d1192f731acfa09303bd1
Normuminov <normuminovbilol@gmail.com>
2020-10-30 23:26:17 +0500

    Created hi.py, square.py

---
+++ turtle/square.py
@@ -0,0 +1,35 @@
+
+from turtle import *
+
+# change line width
+pensize(5)
+
+# change to an actual turtle
+shape("turtle")
+
+# Go to first position
+penup()
+left(180)
+forward(50)
+right(90)
+forward(50)
+
+# Draw top line
+pendown()
+right(90)
+forward(100)
+
+# Draw right line
+right(90)
+forward(100)
+
+# Draw bottom line 
+right(90)
+forward(100)
+
+# Draw left line
+right(90)
+forward(100)
+
+done()
+
