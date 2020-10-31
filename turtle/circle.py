1f3fce9ab7c701753c8a45c55ee8fa9de457dc03
Normuminov <normuminovbilol@gmail.com>
2020-10-31 12:21:29 +0500

    Created circle.py, triangle.py

---
+++ turtle/circle.py
@@ -0,0 +1,15 @@
+
+# circle.py
+from turtle import *
+
+# change line width
+pensize(5)
+
+# draw a circle with a radius of 50 pixels
+circle(50)
+
+# draw a semicircle with a radius of 100 pixels
+circle(100, extent=180)
+
+done()
+
---
+++ turtle/triangle.py
@@ -0,0 +1,11 @@
+
+# triangle.py
+from turtle import *
+
+# change line width
+pensize(5)
+
+circle(100, steps=3)
+
+done()
+
