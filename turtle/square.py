e1189f811480ff288dafb95d0c2e4dfef4eeeacf
Normuminov <normuminovbilol@gmail.com>
2020-10-31 12:25:29 +0500

    Modified square.py
    Enhanced code with turtle.circle function

--- turtle/square.py
+++ turtle/square.py
@@ -7,29 +7,8 @@ pensize(5)
 # change to an actual turtle
 shape("turtle")
 
-# Go to first position
-penup()
-left(180)
-forward(50)
-right(90)
-forward(50)
-
-# Draw top line
-pendown()
-right(90)
-forward(100)
-
-# Draw right line
-right(90)
-forward(100)
-
-# Draw bottom line 
-right(90)
-forward(100)
-
-# Draw left line
-right(90)
-forward(100)
+# drawing a square
+circle(50, steps=4)
 
 done()
 
